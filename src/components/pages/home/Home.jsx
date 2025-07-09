eec9319d46eab677df5a0fa10db5aa61bcb168de
LandonLorica5555 <landonlorica41@gmail.com>
2025-07-10 07:00:35 +0800

    first commit

---
+++ src/components/pages/home/Home.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Navigation from "../../partials/Navigation";
+import Hero from "./hero/Hero";
+import Features from "./features/Features";
+import Testimonials from "./testimonials/Testimonials";
+import Pricing from "./pricing/Pricing";
+import Cta from "./cta/Cta";
+import Footer from "../../partials/Footer";
+
+const Home = () => {
+  return (
+    <>
+      <div className="min-h-screen bg-gray-100">
+        <Navigation />
+        <Hero />
+        <Features />
+        <Testimonials />
+        <Pricing />
+        <Cta />
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default Home;
